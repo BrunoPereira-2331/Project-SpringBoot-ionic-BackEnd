0095dbe53af3a435b5d3454b731032b7561c52b1
Bruno Pereira <Brunopereira.19@hotmail.com>
2020-01-18 09:17:32 -0300

    Project created

---
+++ src/test/java/com/bruno/projectMc/ProjectMcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.projectMc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
