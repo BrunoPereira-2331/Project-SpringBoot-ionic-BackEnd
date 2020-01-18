2b02260bd76cbbae05eb2eb3f0d7912e35686e30
Bruno Pereira <Brunopereira.19@hotmail.com>
2020-01-18 09:38:20 -0300

    first entity created

--- src/main/java/com/bruno/projectMc/resources/CategoryResource.java
+++ src/main/java/com/bruno/projectMc/resources/CategoryResource.java
@@ -1,15 +1,27 @@
 package com.bruno.projectMc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bruno.projectMc.entities.Category;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
 
 	@RequestMapping(method= RequestMethod.GET)
-	public String listar( ) {
-		return "ok";
+	
+	public List<Category> listar( ) {
+		Category cat1 = new Category(1, "Informatica");
+		Category cat2 = new Category(2, "Escritorio");
+		List<Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 }
