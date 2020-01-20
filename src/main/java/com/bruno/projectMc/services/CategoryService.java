fdec824e180aee38a87671be8d4ea66310c3b357
Bruno Pereira <Brunopereira.19@hotmail.com>
2020-01-20 17:01:40 -0300

    delete category using DELETE

--- src/main/java/com/bruno/projectMc/services/CategoryService.java
+++ src/main/java/com/bruno/projectMc/services/CategoryService.java
@@ -3,10 +3,12 @@ package com.bruno.projectMc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.bruno.projectMc.domain.Category;
 import com.bruno.projectMc.repositories.CategoryRepository;
+import com.bruno.projectMc.services.exceptions.DataIntegrityException;
 import com.bruno.projectMc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -32,4 +34,13 @@ public class CategoryService {
 		return repo.save(obj);
 	}
 	
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos");
+		}
+	}
 }
---
+++ src/main/java/com/bruno/projectMc/services/exceptions/DataIntegrityException.java
@@ -0,0 +1,15 @@
+package com.bruno.projectMc.services.exceptions;
+
+public class DataIntegrityException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public DataIntegrityException(String msg) {
+		super(msg);
+	}
+	
+	public DataIntegrityException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+	
+}
