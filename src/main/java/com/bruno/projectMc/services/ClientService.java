f969f0c92049a2c8a8fcd74150a382ad329ddebc
Bruno Pereira <Brunopereira.19@hotmail.com>
2020-01-21 14:57:52 -0300

    PUT, DELETE & GET for clients

--- src/main/java/com/bruno/projectMc/services/ClientService.java
+++ src/main/java/com/bruno/projectMc/services/ClientService.java
@@ -1,23 +1,63 @@
 package com.bruno.projectMc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.bruno.projectMc.domain.Client;
+import com.bruno.projectMc.dto.ClientDTO;
 import com.bruno.projectMc.repositories.ClientRepository;
+import com.bruno.projectMc.services.exceptions.DataIntegrityException;
 import com.bruno.projectMc.services.exceptions.ObjectNotFoundException;
 
 @Service
-public class ClientService{
+public class ClientService {
 
 	@Autowired
 	private ClientRepository repo;
-	
+
 	public Client find(Integer id) {
 		Optional<Client> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-		"Objeto não encontrado! Id: " + id + ", Tipo: " + Client.class.getName()));
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Client.class.getName()));
+	}
+
+	public Client update(Client obj) {
+		Client newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos");
 		}
+	}
+
+	public List<Client> findAll() {
+		return repo.findAll();
+	}
+
+	public Page<Client> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+
+	public Client fromDTO(ClientDTO objDto) {
+		return new Client(objDto.getId(), objDto.getName(), objDto.getEmail(), null, null);
+	}
+	
+	private void updateData(Client newObj, Client obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
 }
