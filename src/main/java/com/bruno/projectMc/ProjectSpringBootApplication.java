747c0c236bb60ee1ecd6ee485892e72c5ff4b159
Bruno Pereira <Brunopereira.19@hotmail.com>
2020-02-04 21:58:40 -0300

    saving the first file in S3

--- src/main/java/com/bruno/projectMc/ProjectSpringBootApplication.java
+++ src/main/java/com/bruno/projectMc/ProjectSpringBootApplication.java
@@ -1,19 +1,25 @@
 package com.bruno.projectMc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.bruno.projectMc.services.S3Service;
+
 @SpringBootApplication
 public class ProjectSpringBootApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectSpringBootApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		s3Service.uploadFile("C:\\temporario\\aa.jpg");
 	}
 
 }
